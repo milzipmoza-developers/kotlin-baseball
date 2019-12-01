7c168661e0122999dbedb53eae0d59b5d24ef36c
mrkwon <erased11@gmail.com>
2019-12-01 22:37:40 +0900

    refactor: Board 를 리팩토링 한다.

--- src/main/kotlin/dev/milzipmoza/model/Board.kt
+++ src/main/kotlin/dev/milzipmoza/model/Board.kt
@@ -6,45 +6,26 @@ import dev.milzipmoza.model.exception.PointUpperBoundException
 private const val BOARD_SIZE = 3
 private const val POINT_LOWER_BOUND = 0
 private const val POINT_UPPER_BOUND = 2
-private const val POINT_X = "Point X"
-private const val POINT_Y = "Point Y"
-private const val POINTS = "Point X and Y"
 
 class Board {
 
     private val board = Array(BOARD_SIZE) { Line() }
 
     fun putPiece(pointX: Int, pointY: Int, piece: Piece): Piece {
-        checkLowerBound(pointX, pointY)
-        checkUpperBound(pointX, pointY)
+        checkLowerBound(pointX)
+        checkUpperBound(pointX)
         return board[pointX].putPiece(pointY, piece)
     }
 
-    private fun checkLowerBound(pointX: Int, pointY: Int) {
-        if (pointX < POINT_LOWER_BOUND || pointY < POINT_LOWER_BOUND) {
-            throw PointLowerBoundException(lowerBoundWhichPoint(pointX, pointY))
+    private fun checkLowerBound(pointX: Int) {
+        if (pointX < POINT_LOWER_BOUND) {
+            throw PointLowerBoundException()
         }
     }
 
-    private fun lowerBoundWhichPoint(pointX: Int, pointY: Int): String {
-        return when {
-            pointX < POINT_LOWER_BOUND -> POINT_X
-            pointY < POINT_LOWER_BOUND -> POINT_Y
-            else -> POINTS
-        }
-    }
-
-    private fun checkUpperBound(pointX: Int, pointY: Int) {
-        if (pointX > POINT_UPPER_BOUND || pointY > POINT_UPPER_BOUND) {
-            throw PointUpperBoundException(upperBoundWhichPoint(pointX, pointY))
-        }
-    }
-
-    private fun upperBoundWhichPoint(pointX: Int, pointY: Int): String {
-        return when {
-            pointX > POINT_UPPER_BOUND -> POINT_X
-            pointY > POINT_UPPER_BOUND -> POINT_Y
-            else -> POINTS
+    private fun checkUpperBound(pointX: Int) {
+        if (pointX > POINT_UPPER_BOUND) {
+            throw PointUpperBoundException()
         }
     }
 }
--- src/test/kotlin/dev/milzipmoza/model/BoardTest.kt
+++ src/test/kotlin/dev/milzipmoza/model/BoardTest.kt
@@ -3,7 +3,7 @@ package dev.milzipmoza.model
 import dev.milzipmoza.model.exception.PointLowerBoundException
 import dev.milzipmoza.model.exception.PointUpperBoundException
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
@@ -12,11 +12,6 @@ private const val POINT_UNDER_LOWER_BOUND = -1
 private const val POINT_OVER_UPPER_BOUND = 3
 private const val POINT_LOWER_BOUND = 0
 private const val POINT_UPPER_BOUND = 2
-
-private const val POINT_X_LOWER_BOUND_MESSAGE = "Point X 는 0보다 낮은 수를 지정할 수 없습니다."
-private const val POINT_Y_LOWER_BOUND_MESSAGE = "Point Y 는 0보다 낮은 수를 지정할 수 없습니다."
-private const val POINT_X_UPPER_BOUND_MESSAGE = "Point X 는 2보다 큰 수를 지정할 수 없습니다."
-private const val POINT_Y_UPPER_BOUND_MESSAGE = "Point Y 는 2보다 큰 수를 지정할 수 없습니다."
 
 internal class BoardTest {
 
@@ -39,37 +34,29 @@ internal class BoardTest {
 
     @Test
     internal fun `X 가 0 보다 작을 경우 PointLowerBoundException 이 발생한다` () {
-        val exception = assertThrows<PointLowerBoundException> {
+        assertThrows<PointLowerBoundException> {
             board.putPiece(POINT_UNDER_LOWER_BOUND, POINT_LOWER_BOUND, Piece.O)
         }
-
-        assertThat(exception.message).isEqualTo(POINT_X_LOWER_BOUND_MESSAGE)
     }
 
     @Test
     internal fun `Y 가 0 보다 작을 경우 PointLowerBoundException 이 발생한다` () {
-        val exception = assertThrows<PointLowerBoundException> {
+        assertThrows<PointLowerBoundException> {
             board.putPiece(POINT_LOWER_BOUND, POINT_UNDER_LOWER_BOUND, Piece.O)
         }
-
-        assertThat(exception.message).isEqualTo(POINT_Y_LOWER_BOUND_MESSAGE)
     }
 
     @Test
     internal fun `X 가 2 보다 클 경우 PointUpperBoundException 이 발생한다` () {
-        val exception = assertThrows<PointUpperBoundException> {
+        assertThrows<PointUpperBoundException> {
             board.putPiece(POINT_OVER_UPPER_BOUND, POINT_UPPER_BOUND, Piece.O)
         }
-
-        assertThat(exception.message).isEqualTo(POINT_X_UPPER_BOUND_MESSAGE)
     }
 
     @Test
     internal fun `Y 가 2 보다 클 경우 PointUpperBoundException 이 발생한다` () {
-        val exception = assertThrows<PointUpperBoundException> {
+        assertThrows<PointUpperBoundException> {
             board.putPiece(POINT_UPPER_BOUND, POINT_OVER_UPPER_BOUND, Piece.O)
         }
-
-        assertThat(exception.message).isEqualTo(POINT_Y_UPPER_BOUND_MESSAGE)
     }
 }
