61a8b6eac91be26090606d760d4db3e62afdc9e2
mrkwon <erased11@gmail.com>
2019-12-01 23:32:42 +0900

    refactor: Piece 와 PieceTest 를 리팩토링 한다.

--- src/main/kotlin/dev/milzipmoza/model/Piece.kt
+++ src/main/kotlin/dev/milzipmoza/model/Piece.kt
@@ -5,10 +5,8 @@ private const val EMPTY_SYMBOL = " "
 enum class Piece {
     EMPTY, O, X;
 
-    fun symbolize(): String {
-        return when (EMPTY) {
-            this -> EMPTY_SYMBOL
-            else -> this.name
-        }
+    fun symbolize() = when (EMPTY) {
+        this -> EMPTY_SYMBOL
+        else -> this.name
     }
 }
--- src/test/kotlin/dev/milzipmoza/model/PieceTest.kt
+++ src/test/kotlin/dev/milzipmoza/model/PieceTest.kt
@@ -3,26 +3,30 @@ package dev.milzipmoza.model
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
+private const val EMPTY_PIECE = " "
+private const val O_PIECE = "O"
+private const val X_PIECE = "X"
+
 internal class PieceTest {
 
     @Test
     internal fun `Piece 의 EMPTY 에 대하여 symbolize() 를 확인한다` () {
         val emptyPiece = Piece.EMPTY
 
-        assertThat(emptyPiece.symbolize()).isEqualTo(" ")
+        assertThat(emptyPiece.symbolize()).isEqualTo(EMPTY_PIECE)
     }
 
     @Test
     internal fun `Piece 의 O 에 대하여 symbolize() 를 확인한다` () {
         val pieceO = Piece.O
 
-        assertThat(pieceO.symbolize()).isEqualTo("O")
+        assertThat(pieceO.symbolize()).isEqualTo(O_PIECE)
     }
 
     @Test
     internal fun `Piece 의 X 에 대하여 symbolize() 를 확인한다` () {
         val pieceX = Piece.X
 
-        assertThat(pieceX.symbolize()).isEqualTo("X")
+        assertThat(pieceX.symbolize()).isEqualTo(X_PIECE)
     }
 }
