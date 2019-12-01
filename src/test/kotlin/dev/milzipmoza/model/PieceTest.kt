e4f0f7d821fae32073b62eee46ca3fe88664f982
mrkwon <erased11@gmail.com>
2019-12-02 00:38:00 +0900

    test: nextTurn 메서드 테스트를 추가한다.

--- src/test/kotlin/dev/milzipmoza/model/PieceTest.kt
+++ src/test/kotlin/dev/milzipmoza/model/PieceTest.kt
@@ -1,7 +1,9 @@
 package dev.milzipmoza.model
 
+import dev.milzipmoza.model.exception.InvalidTurnException
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 private const val EMPTY_PIECE = " "
 private const val O_PIECE = "O"
@@ -28,5 +30,22 @@ internal class PieceTest {
         val pieceX = Piece.X
 
         assertThat(pieceX.symbolize()).isEqualTo(X_PIECE)
+    }
+
+    @Test
+    internal fun `O 일 때 changeTurn 이 성공한다`() {
+        assertThat(Piece.O.nextTurn()).isEqualTo(Piece.X)
+    }
+
+    @Test
+    internal fun `X 일 때 changeTurn 이 성공한다`() {
+        assertThat(Piece.X.nextTurn()).isEqualTo(Piece.O)
+    }
+
+    @Test
+    internal fun `EMPTY 일 때 changeTurn 이 InvalidTurnException 을 발생시킨다`() {
+        assertThrows<InvalidTurnException> {
+            Piece.EMPTY.nextTurn()
+        }
     }
 }
