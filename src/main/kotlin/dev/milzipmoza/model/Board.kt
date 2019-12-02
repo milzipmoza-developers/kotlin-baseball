02308315845da6a4e2582f85c6562ec6af275acd
mrkwon <erased11@gmail.com>
2019-12-03 02:05:20 +0900

    feat: Board 에 승자를 구하는 메서드를 추가한다.
    - hasVerticalWinner
    - hasHorizontalWinner
    - hasDiagonalWinner

--- src/main/kotlin/dev/milzipmoza/model/Board.kt
+++ src/main/kotlin/dev/milzipmoza/model/Board.kt
@@ -4,11 +4,17 @@ import dev.milzipmoza.model.exception.IllegalFirstTurnException
 import dev.milzipmoza.model.exception.NotYourTurnException
 import dev.milzipmoza.model.exception.PointLowerBoundException
 import dev.milzipmoza.model.exception.PointUpperBoundException
+import java.util.stream.IntStream
 
 private const val BOARD_SIZE = 3
 private const val POINT_LOWER_BOUND = 0
+private const val POINT_MIDDLE = 1
 private const val POINT_UPPER_BOUND = 2
 private const val JOINING_DELIMITER = "\n"
+
+private const val FIRST_POINT = 0
+private const val SECOND_POINT = 1
+private const val THIRD_POINT = 2
 
 class Board(internal var turn: Piece) {
     private val board = Array(BOARD_SIZE) { Line() }
@@ -25,6 +31,34 @@ class Board(internal var turn: Piece) {
     }
 
     fun symbolize() = board.asSequence().joinToString(JOINING_DELIMITER) { it.symbolize() }
+
+    fun hasVerticalWinner() = lowerBoundToUpperBound().anyMatch { isVerticalWinner(it) }
+
+    fun hasHorizontalWinner() = board.asSequence().any { it.hasWinner() }
+
+    fun hasDiagonalWinner() =
+        IntStream.rangeClosed(POINT_LOWER_BOUND, POINT_MIDDLE).anyMatch {
+            checkRightDiagonal(it) || checkLeftDiagonal(it)
+        } && !isMiddleEmpty()
+
+    private fun lowerBoundToUpperBound() = IntStream.rangeClosed(POINT_LOWER_BOUND, POINT_UPPER_BOUND)
+
+    private fun checkRightDiagonal(index: Int) =
+        board[index] at index == board[index + 1] at index + 1
+
+    private fun checkLeftDiagonal(index: Int) =
+        board[2 - index] at index == board[index + 1] at 2 - (index + 1)
+
+    private fun isMiddleEmpty() = (board[SECOND_POINT] at SECOND_POINT).isEmpty()
+
+    private fun isVerticalWinner(verticalIdx: Int) =
+        isVerticalSame(verticalIdx) && !hasVerticalSecondPoint(verticalIdx)
+
+    private fun isVerticalSame(verticalIdx: Int) =
+        board[FIRST_POINT] at verticalIdx == board[SECOND_POINT] at verticalIdx
+                && board[SECOND_POINT] at verticalIdx == board[THIRD_POINT] at verticalIdx
+
+    private fun hasVerticalSecondPoint(verticalIdx: Int) = (board[SECOND_POINT] at verticalIdx).isEmpty()
 
     private fun updateBoard(pointX: Int, pointY: Int, piece: Piece): Piece {
         val updatedPiece = board[pointX].putPiece(pointY, piece)
--- src/test/kotlin/dev/milzipmoza/model/BoardTest.kt
+++ src/test/kotlin/dev/milzipmoza/model/BoardTest.kt
@@ -21,50 +21,50 @@ internal class BoardTest {
     private lateinit var board: Board
 
     @BeforeEach
-    internal fun setUp() {
+    fun setUp() {
         board = Board(START_TURN)
     }
 
     @Test
-    internal fun `틱택토 게임을 진행하기 위한 Board 를 생성한다`() {
+    fun `틱택토 게임을 진행하기 위한 Board 를 생성한다`() {
         assertNotNull(board)
     }
 
     @Test
-    internal fun `새로운 Piece 를 성공적으로 배치한다`() {
+    fun `새로운 Piece 를 성공적으로 배치한다`() {
         assertThat(board.putPiece(POINT_LOWER_BOUND, POINT_LOWER_BOUND, Piece.O)).isEqualTo(Piece.O)
     }
 
     @Test
-    internal fun `X 가 0 보다 작을 경우 PointLowerBoundException 이 발생한다`() {
+    fun `X 가 0 보다 작을 경우 PointLowerBoundException 이 발생한다`() {
         assertThrows<PointLowerBoundException> {
             board.putPiece(POINT_UNDER_LOWER_BOUND, POINT_LOWER_BOUND, Piece.O)
         }
     }
 
     @Test
-    internal fun `Y 가 0 보다 작을 경우 PointLowerBoundException 이 발생한다`() {
+    fun `Y 가 0 보다 작을 경우 PointLowerBoundException 이 발생한다`() {
         assertThrows<PointLowerBoundException> {
             board.putPiece(POINT_LOWER_BOUND, POINT_UNDER_LOWER_BOUND, Piece.O)
         }
     }
 
     @Test
-    internal fun `X 가 2 보다 클 경우 PointUpperBoundException 이 발생한다`() {
+    fun `X 가 2 보다 클 경우 PointUpperBoundException 이 발생한다`() {
         assertThrows<PointUpperBoundException> {
             board.putPiece(POINT_OVER_UPPER_BOUND, POINT_UPPER_BOUND, Piece.O)
         }
     }
 
     @Test
-    internal fun `Y 가 2 보다 클 경우 PointUpperBoundException 이 발생한다`() {
+    fun `Y 가 2 보다 클 경우 PointUpperBoundException 이 발생한다`() {
         assertThrows<PointUpperBoundException> {
             board.putPiece(POINT_UPPER_BOUND, POINT_OVER_UPPER_BOUND, Piece.O)
         }
     }
 
     @Test
-    internal fun `init 된 Board 의 symbolize 메서드 동작을 확인한다`() {
+    fun `init 된 Board 의 symbolize 메서드 동작을 확인한다`() {
         val symbolizeBoard = board.symbolize()
         println(symbolizeBoard)
 
@@ -76,12 +76,8 @@ internal class BoardTest {
     }
 
     @Test
-    internal fun `변경사항이 있는 Board 의 symbolize 메서드 동작을 확인한다`() {
-        board.putPiece(0, 0, Piece.O)
-        board.putPiece(0, 2, Piece.X)
-        board.putPiece(1, 1, Piece.O)
-        board.putPiece(1, 2, Piece.X)
-        board.putPiece(2, 2, Piece.O)
+    fun `변경사항이 있는 Board 의 symbolize 메서드 동작을 확인한다`() {
+        makeLeftDiagonalWinner()
 
         val symbolizeBoard = board.symbolize()
         println(symbolizeBoard)
@@ -94,14 +90,14 @@ internal class BoardTest {
     }
 
     @Test
-    internal fun `첫 턴이 아닌 유저가 먼저 움직이면 NotYourTurnException 이 발생한다`() {
+    fun `첫 턴이 아닌 유저가 먼저 움직이면 NotYourTurnException 이 발생한다`() {
         assertThrows<NotYourTurnException> {
             board.putPiece(0, 1, Piece.X)
         }
     }
 
     @Test
-    internal fun `동일한 유저가 연속으로 움직이면 NotYourTurnException 이 발생한다`() {
+    fun `동일한 유저가 연속으로 움직이면 NotYourTurnException 이 발생한다`() {
         board.putPiece(0, 0, Piece.O)
         assertThrows<NotYourTurnException> {
             board.putPiece(0, 1, Piece.O)
@@ -109,9 +105,118 @@ internal class BoardTest {
     }
 
     @Test
-    internal fun `첫 턴을 EMPTY 로 시작하면 IllegalFirstTurnException 이 발생한다`() {
+    fun `첫 턴을 EMPTY 로 시작하면 IllegalFirstTurnException 이 발생한다`() {
         assertThrows<IllegalFirstTurnException> {
             Board(Piece.EMPTY)
         }
+    }
+
+    @Test
+    fun `수직으로 3연속 등장하는 모양이 있으면 hasVerticalWinner 가 true 를 반환한다`() {
+        makeVerticalWinner()
+
+        val symbolizeBoard = board.symbolize()
+        println(symbolizeBoard)
+
+        assertThat(board.hasVerticalWinner()).isTrue()
+    }
+
+    @Test
+    fun `수직으로 3연속 등장하는 모양이 Empty 이면 있으면 hasVerticalWinner 가 false 를 반환한다`() {
+        makeVerticalNoWinnerWithTripleEmpty()
+
+        val symbolizeBoard = board.symbolize()
+        println(symbolizeBoard)
+
+        assertThat(board.hasVerticalWinner()).isFalse()
+    }
+
+    @Test
+    fun `수평으로 3연속 등장하는 모양이 있으면 hasHorizontalWinner 가 true 를 반환한다`() {
+        makeHorizontalWinner()
+
+        val symbolizeBoard = board.symbolize()
+        println(symbolizeBoard)
+
+        assertThat(board.hasHorizontalWinner()).isTrue()
+    }
+
+    @Test
+    fun `수평으로 3연속 등장하는 모양이 Empty 이면 hasHorizontalWinner 가 false 를 반환한다`() {
+        makeHorizontalNoWinnerWithTripleEmpty()
+
+        val symbolizeBoard = board.symbolize()
+        println(symbolizeBoard)
+
+        assertThat(board.hasHorizontalWinner()).isFalse()
+    }
+
+    @Test
+    fun `왼쪽 대각선으로 3연속 등장하는 모양이 있으면 hasDiagonalWinner 가 true 를 반환한다`() {
+        makeLeftDiagonalWinner()
+
+        val symbolizeBoard = board.symbolize()
+        println(symbolizeBoard)
+
+        assertThat(board.hasDiagonalWinner()).isTrue()
+    }
+
+    @Test
+    fun `오른쪽 대각선으로 3연속 등장하는 모양이 있으면 hasDiagonalWinner 가 true 를 반환한다`() {
+        makeRightDiagonalWinner()
+
+        val symbolizeBoard = board.symbolize()
+        println(symbolizeBoard)
+
+        assertThat(board.hasDiagonalWinner()).isTrue()
+    }
+
+    private fun makeLeftDiagonalWinner() {
+        board.putPiece(0, 0, Piece.O)
+        board.putPiece(0, 2, Piece.X)
+        board.putPiece(1, 1, Piece.O)
+        board.putPiece(1, 2, Piece.X)
+        board.putPiece(2, 2, Piece.O)
+    }
+
+    private fun makeRightDiagonalWinner() {
+        board.putPiece(0, 1, Piece.O)
+        board.putPiece(0, 2, Piece.X)
+        board.putPiece(2, 2, Piece.O)
+        board.putPiece(1, 1, Piece.X)
+        board.putPiece(1, 2, Piece.O)
+        board.putPiece(2, 0, Piece.X)
+    }
+
+    private fun makeHorizontalWinner() {
+        board.putPiece(0, 0, Piece.O)
+        board.putPiece(2, 0, Piece.X)
+        board.putPiece(0, 1, Piece.O)
+        board.putPiece(1, 0, Piece.X)
+        board.putPiece(0, 2, Piece.O)
+    }
+
+    private fun makeHorizontalNoWinnerWithTripleEmpty() {
+        board.putPiece(1, 1, Piece.O)
+        board.putPiece(0, 0, Piece.X)
+        board.putPiece(0, 1, Piece.O)
+        board.putPiece(1, 0, Piece.X)
+        board.putPiece(0, 2, Piece.O)
+    }
+
+    private fun makeVerticalWinner() {
+        board.putPiece(0, 1, Piece.O)
+        board.putPiece(0, 2, Piece.X)
+        board.putPiece(1, 1, Piece.O)
+        board.putPiece(1, 2, Piece.X)
+        board.putPiece(2, 1, Piece.O)
+    }
+
+    private fun makeVerticalNoWinnerWithTripleEmpty() {
+        board.putPiece(0, 1, Piece.O)
+        board.putPiece(0, 2, Piece.X)
+        board.putPiece(1, 1, Piece.O)
+        board.putPiece(2, 1, Piece.X)
+        board.putPiece(1, 2, Piece.O)
     }
 }
